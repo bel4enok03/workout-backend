a840f03aeaf03c6c94c50d38e6d8234c0d3a91fc
bel4enok03 <belskaya-tanyush@mail.ru>
2023-05-15 23:39:54 +0300

    изменения логин на емаил

--- controlles/authController.js
+++ controlles/authController.js
@@ -5,16 +5,16 @@ const jwt = require('jsonwebtoken');
 
 const register = async (req, res) => {
 	try {
-		const { username, password } = req.body;
+		const { email, password } = req.body; // Изменено с username на email
 
-		const existingUser = await User.findOne({ username });
+		const existingUser = await User.findOne({ email }); // Изменено с username на email
 		if (existingUser) {
-			return res.status(400).json({ message: 'Username already exists' });
+			return res.status(400).json({ message: 'Email already exists' }); // Изменено с Username на Email
 		}
 
 		const hashedPassword = await bcrypt.hash(password, 10);
 
-		const user = await User.create({ username, password: hashedPassword });
+		const user = await User.create({ email, password: hashedPassword }); // Изменено с username на email
 
 		res.status(201).json({ message: 'User registered successfully' });
 	} catch (error) {
@@ -25,9 +25,9 @@ const register = async (req, res) => {
 
 const login = async (req, res) => {
 	try {
-		const { username, password } = req.body;
+		const { email, password } = req.body; // Изменено с username на email
 
-		const user = await User.findOne({ username });
+		const user = await User.findOne({ email }); // Изменено с username на email
 		if (!user) {
 			return res.status(400).json({ message: 'Invalid credentials' });
 		}
--- models/User.js
+++ models/User.js
@@ -1,7 +1,8 @@
+// User.js
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-	username: { type: String, required: true },
+	email: { type: String, required: true },  // Изменено с username на email
 	password: { type: String, required: true },
 });
 
