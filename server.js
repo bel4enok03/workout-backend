98dea5fd5e8bee415e74db6a112c740dc6b67dd6
bel4enok03 <belskaya-tanyush@mail.ru>
2023-05-16 15:29:49 +0300

    *

--- server.js
+++ server.js
@@ -5,33 +5,43 @@ const cors = require('cors');
 const workoutRouter = require('./routes/workoutRoutes');
 const authRouter = require('./routes/authRoutes');
 
+require('dotenv').config();
+mongoose.set('strictQuery', false);
+
+const PORT = process.env.PORT || 8000;
+const MONGODB_URI = process.env.MONGODB_URI;
+
 app.use(
 	cors({
 		origin: 'https://workouts-plan.netlify.app',
 	})
 );
-
-require('dotenv').config();
-mongoose.set('strictQuery', false);
-
-const PORT = 8000 || process.env.port;
-
 app.use(express.json());
+
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', 'https://workouts-plan.netlify.app');
+	res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+	next();
+});
+
+// Routes
 app.use('/', workoutRouter);
 app.use('/auth', authRouter);
 app.use('/workouts', workoutRouter);
 
-mongoose
-	.connect(process.env.MONGODB_LINK)
-	.then(() => console.log(`Database connected`))
-	.catch((err) => console.log(err));
-
 app.use((err, req, res, next) => {
 	console.error(err.stack);
-
 	res.status(500).send('Server Error');
 });
 
-app.listen(PORT, () => {
-	console.log(`I'm listening on PORT ${PORT}`);
-});
+mongoose
+	.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() => {
+		console.log('Connected to MongoDB');
+		// Start the server
+		app.listen(PORT, () => {
+			console.log(`Server is running on port ${PORT}`);
+		});
+	})
+	.catch((err) => console.error(err));
